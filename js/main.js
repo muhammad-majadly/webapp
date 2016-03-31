4af3d3f9f97d7c06532e37818c71fca1d5b93986
majadly <mu.ib.mj@gmail.com>
2016-03-31 16:24:51 +0300

    commit7

--- js/main.js
+++ js/main.js
@@ -15,6 +15,8 @@ function updateNotification (notification) {
 	}
 }
 
+
+
 function updateActions (data) {
 	if(data !==undefined){
 		
@@ -66,6 +68,34 @@ function hideElem1()
 	else
 	{
 	    document.getElementById("choose-sites").style.visibility = "hidden"; 
+	}
+
+	if(document.getElementById("choose-sites").style.display == "none")
+	{
+	    document.getElementById("choose-sites").style.display = "block"; 
+	}
+
+	
+}
+
+function hideElem2()
+{
+ 	
+ 	//document.getElementById('quick-reports').innerHTML='<h2>HelloWorld</h2>';
+
+
+	if(document.getElementById("choose-sites2").style.visibility == "hidden")
+	{
+	    document.getElementById("choose-sites2").style.visibility = "visible"; 
+	}
+	else
+	{
+	    document.getElementById("choose-sites2").style.visibility = "hidden"; 
+	}
+
+	if(document.getElementById("choose-sites2").style.display == "none")
+	{
+	    document.getElementById("choose-sites2").style.display = "block"; 
 	}
 
 	
@@ -122,6 +152,9 @@ function quickReport()
 	    document.getElementById("my-team-folders").style.display = "none";
 
 
+	    document.getElementById("choose-sites").style.visibility = "hidden"; 
+
+
 	    if(document.getElementById("tab-1").className == "tabs-li") 
 	    {
 	    	document.getElementById("tab-1").className += " active";
@@ -135,13 +168,14 @@ function quickReport()
 function myTeamFolders()
 {
 	
-
 		document.getElementById("my-team-folders").style.display = "block";
 		document.getElementById("reports-iframe2").style.visibility = "hidden";
 		document.getElementById('quick-reports').style.display = "none";
 	    document.getElementById("my-folders").style.display = "none";
 	    document.getElementById("public-folders").style.display = "none";
-	    
+
+	    document.getElementById("choose-sites2").style.visibility = "hidden"; 
+
 
 	    if(document.getElementById("tab-3").className == "tabs-li") 
 	    {
@@ -155,11 +189,19 @@ function myTeamFolders()
 function saveUrl() {
 	// body...
 	
-	var v1=document.getElementById('url1').value;
-	document.getElementById('reports-iframe').src=v1;
+	var url=document.getElementById('url1').value;
+	var name=document.getElementById('name1').value;
+
+	document.getElementById('reports-iframe').src=url;
+	document.getElementById('href').href=url;
+	document.getElementById('n1').innerHTML=name;
+
 	document.getElementById("reports-iframe").style.visibility = "visible";
 	document.getElementById("choose-sites").style.display = "none";
 }
+
+
+
 
 function loadPageData(response){
 	updateNotification(response.notification);
@@ -176,22 +218,31 @@ function loadQuickActions(data){
 }*/
 function init()
 {
+	var flag=0;
 	var ref=this.hash || window.location.hash;
 	if (ref=="#my-team-folders") {
 		myTeamFolders();
+		flag=1;
 	}
 	if (ref=="#quick-reports") {
 		quickReport();
+		flag=1;
 	}
 
 	if (ref=="#my-folders") {
 		myFolder();
+		flag=1;
 	}
 
 	if (ref=="#public-folders") {
 		publicFolders();
+		flag=1;
 	}
 	
+	if (flag==0) 
+	{
+		quickReport();
+	}
 	
 
 	UTILS.ajax("data/config.json",{done:loadPageData});
@@ -201,7 +252,59 @@ function init()
 
 }
 
-// var ref=this.hash || window.location.hash;
+
+/*
+function savelinksReports () {
+    var names=[];
+    var url=[];
+    var array=[];
+    names = all(".reportname");
+    url = all(".reporturl");
+
+    
+    for (var i=0;i<3;i++)
+    {
+        
+        var rn = names[i].children[1].value;
+        var ru = url[i].children[1].value;
+       // if(rn!=""&&ru!=""){   
+            array.push({
+                    "name":rn,
+                    "url":ru
+            });
+        //}
+        
+    }
+    var linkarray = JSON.parse(localStorage.getItem("linkarray"));
+    if(linkarray==null){
+        linkarray=[];
+        for (i=0;i<3;i++)
+        {
+        linkarray.push({
+                "name":"",
+                "url":""
+
+        });
+        }
+    }
+    $("#quick-reports-adress").innerHTML="";
+     for (i=0;i<3;i++)
+    {
+        linkarray[i].name=array[i].name;
+        linkarray[i].url=array[i].url;
+        if(array[i].name!="" && array[i].url!="" ){
+            $("#quick-reports-adress").innerHTML+="<option value='"+linkarray[i].url+"'>"+linkarray[i].name+"</option>";
+        }
+    }
+    localStorage.setItem("linkarray" , JSON.stringify(linkarray));
+    setReportAdress();
+    //var link = JSON.parse(localStorage.getItem("linkarray"));
+        //updatelinksReports();
+
+}
+
+
+*/
 
 
 
